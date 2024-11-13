ae9575199f859763a2f9e6bd9cc27b6d4141c203
mani-prakash-007 <manip8072@gmail.com>
2024-11-13 17:11:43 +0530

    Added URL Image in head icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { UrlShortner } from "./components/pages/UrlShortner";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center my-10">
         <h1 className="text-4xl font-bold text-center">URL Shortner</h1>
         <UrlShortner />
       </div>
